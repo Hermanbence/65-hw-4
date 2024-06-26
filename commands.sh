46a2fccd341e828815e6ffd8e588ea8841622650
Herman.B <Herman.B@zdf.de>
2024-06-26 23:26:18 +0200

    labeling fix

--- commands.sh
+++ commands.sh
@@ -1,7 +1,29 @@
-kubectl create ns hw4-dev
+# Commands for kustomize
 
+# Create the namespaces
+kubectl create namespace hw4-dev
+kubectl create ns hw4-prod
+kubectl create ns hw4-test
+
+# Rendering
 kubectl kustomize .
 
+# Apply kustomization yaml in the current dir.
 kubectl apply -k .
+
+# Apply kustomization yaml in the overlay/test dir from parent dir.
 kubectl apply -k .\overlays\test\
+
+# Delete kustomization yaml in the current dir.
 kubectl delete -k .
+#########################################################################
+# Commands for helm
+
+# Create helm chart.
+helm create helm
+
+# Render a Helm chart into Kubernetes manifests 
+helm template .
+
+# 
+helm install hw4-dev .\hw4\ -n hw4-helm
